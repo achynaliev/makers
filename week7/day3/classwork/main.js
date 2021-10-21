5d931f8a369873460b315d2569be2ed57bb43b7f
Atai Chynaliev <chynaliev.ae@gmail.com>
2021-10-21 15:07:51 +0600

    oop done" " " "

--- week7/day3/classwork/main.js
+++ week7/day3/classwork/main.js
@@ -1,138 +1,98 @@
-let API = "http://localhost:3000/products?_page=1&_limit=3";
+let arrOfWords = [
+  {
+    word: "makaka",
+    desc: "Порода обезьяны",
+  },
+  {
+    word: "wkola",
+    desc: "Овощь",
+  },
+  {
+    word: "university",
+    desc: "11 лет",
+  },
+  {
+    word: "class",
+    desc: "носить на плече",
+  },
+  {
+    word: "proshka",
+    desc: "Ёмкость для напитков",
+  },
+];
+let textDiv = $(".text-div");
+let arrOfDivs = [
+  $(".verevka"),
+  $(".head-1"),
+  $(".body-1"),
+  $(".r-hand"),
+  $(".l-hand"),
+  $(".r-foot"),
+  $(".l-foot"),
+];
+let countMistakes = 0;
 
-let productName = $("#productName");
-let productPrice = $("#productPrice");
-let productDesc = $("#productDesc");
-let btnSave = $(".btnSave");
-let modal = $(".modal");
-let productImg = $("#productImg");
-let productURL = $("#productURL");
-let prev = $(".prev");
-let next = $(".next");
-let currentPage = 1;
+function render1() {
+  let randomObject =
+    arrOfWords[Math.round(Math.random() * (arrOfWords.length - 1))];
+  console.log(randomObject);
+  let word = randomObject.word;
+  let desc = randomObject.desc;
+  arrOfDivs.forEach((item) => {
+    item.css("display", "none");
+  });
 
-async function addProduct() {
-  let name = productName.val();
-  let price = productPrice.val();
-  let desc = productDesc.val();
-  let img = productDesc.val();
+  countMistakes = 0;
 
-  let product = {
-    name,
-    price,
-    desc,
-    img,
-  };
-
-  try {
-    const response = await axios.post(API, product);
-    console.log(response);
-    Toastify({
-      text: response.statusText,
-      duration: 1000,
-      newWindow: true,
-      close: true,
-      gravity: "top", // `top` or `bottom`
-      position: "center", // `left`, `center` or `right`
-      stopOnFocus: true, // Prevents dismissing of toast on hover
-      style: {
-        background: "linear-gradient(to right, #00b09b, #96c93d)",
-      },
-    }).showToast();
-    modal.modal("hide");
-    $("body").removeClass("modal-open");
-    $(".modal-backdrop").remove();
-  } catch (err) {
-    Toastify({
-      text: err.response.statusText,
-      duration: 1000,
-      newWindow: true,
-      close: true,
-      gravity: "top", // `top` or `bottom`
-      position: "center", // `left`, `center` or `right`
-      stopOnFocus: true, // Prevents dismissing of toast on hover
-      style: {
-        background: "red",
-      },
-    }).showToast();
-    modal.modal("toggle");
-  }
-}
-
-btnSave.click(function (e) {
-  e.preventDefault();
-  addProduct();
-});
-
-let list = $(".list");
-
-async function render(url) {
-  try {
-    const response = await axios(url);
-    console.log(response.headers.link);
-    list.html("");
-    response.data.forEach((item) => {
-      list.append(`
-  <div class="card" style="width: 18rem;">
-<img style="width: 100%; object-fit: contain; height: 190px" src=${item.image} class="card-img-top" alt="...">
-<div class="card-body">
-  <h5 class="card-title">${item.name} </h5>
-  <p class="card-text">${item.desc}</p>
-  <a href="#">${item.price}</a>
-  <button id="${item.id}" type="button class="btn btn-primary editBtn" data-bs-toggle="modal" data-bs-target="#editModal">Edit product</button>
-</div>
-</div>
-  `);
-    });
-
-    let links = response.headers.link.match(
-      /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/gim
+  for (let i = 0; i < word.length; i++) {
+    textDiv.append(
+      `<input type="text" maxlength="1" class="text" id="text-${i}" value="x" />`
     );
-    console.log(links.length);
-    if (!links) {
-      prev.attr("disabled", "true");
-      next.attr("disabled", "true");
+  }
+  $("h3").text(`${word}`);
+  $(document).on("keypress", function (e) {
+    e.stopImmediatePropagation();
+    e.preventDefault();
+    let letter = e.key;
+
+    let hz = false;
+    for (let i = 0; i < word.length; i++) {
+      if (letter === word[i]) {
+        $(`#text-${i}`).val(`${letter}`);
+        $(`#text-${i}`).css("color", "blue");
+        hz = true;
+      }
+    }
+
+    if (hz === false) {
+      arrOfDivs[countMistakes].css("display", "block");
+      countMistakes++;
+
+      if (countMistakes === 7) {
+        alert(`Вы проиграли, правильное слово: ${word}`);
+        return;
+      }
+    }
+    let bool = true;
+    $(".text").each((i, item) => {
+      if (item.value === "x") {
+        bool = false;
+      }
+    });
+    if (bool) {
+      alert(`Победа, слово: ${word}`);
       return;
     }
-    if (links.length === 4) {
-      prev.attr("id", links[1]);
-      next.attr("id", links[2]);
-      prev.removeAttr("disabled");
-      next.removeAttr("disabled");
-    } else if (links.length === 3 && currentPage === 1) {
-      prev.attr("disabled", "true");
-      next.attr("id", links[1]);
-    } else if (links.length === 3 && currentPage != 1) {
-      next.attr("disabled", "true");
-      prev.attr("id", links[1]);
-    }
-  } catch (e) {
-    console.log(e);
-  }
+    console.log(countMistakes);
+  });
 }
-render(API);
-next.on("click", (e) => {
-  let url = e.target.id;
-  render(url);
-  currentPage++;
-});
-prev.on("click", (e) => {
-  let url = e.target.id;
-  render(url);
-  currentPage--;
-});
 
-// ! Search
-
-let searchInput = $("#inputSearch");
-searchInput.on("input", function (e) {
-  let val = e.target.value;
-  let url = API + `&q=${val}`;
-  render(url);
-  currentPage = 1;
-});
-
-$(document).on("click", ".editBtn", (e) => {
-  let id = e.target.id;
-  console.log(id);
+let btnStart = $("#btn-start");
+btnStart.on("click", function (e) {
+  $("input").empty();
+  $(".text").each((i, item) => {
+    item.text = "";
+  });
+  $(".text-div").empty();
+  render1();
 });
---
+++ week7/oop/main.js
@@ -0,0 +1,288 @@
+// Write Javascript code!
+// ООП. Инкапсуляция.
+// Полиморфизм.
+// Наследование ES6 (классы)
+
+//////////////////////////////////////////
+// Задание №1
+// Реализуйте класс Programmer (Программист),
+// который будет иметь следующие свойства:
+// name (имя),
+// surname (фамилия),
+// programming_language (язык програмирования).
+// Также класс должен иметь метод introduceYourself(),
+// который будет выводить информацию о работнике.
+
+class Programmer {
+  constructor(name, lastname, programming_language) {
+    this.name = name;
+    this.lastname = lastname;
+    this.programming_language = programming_language;
+  }
+  introduceYourself() {
+    return `Name: ${this.name}, Lastname: ${this.lastname}, programming language: ${this.programming_language}`;
+  }
+}
+
+// Вот так должен работать наш класс:
+let programmer = new Programmer("Иван", "Иванов", "Python");
+console.log(programmer.name); //выведет 'Иван'
+console.log(programmer.lastname); //выведет 'Иванов'
+console.log(programmer.programming_language); //выведет 'Python'
+console.log(programmer.introduceYourself()); //выведет 'Меня зовут Иван Иванов и пишу код на языке Python'
+
+//////////////////////////////////////////
+// Задание №2
+// Модифицируйте класс Worker из предыдущей задачи
+// следующим образом: сделайте все его свойства
+// приватными, а для их чтения сделайте методы-геттеры.
+
+class Worker {
+  #name = "hz";
+  #lastname = "hz";
+  #programming_language = "js";
+
+  constructor(name, lastname, programming_language) {
+    this.#name = name;
+    this.#lastname = lastname;
+    this.#programming_language = programming_language;
+  }
+  introduceYourself() {
+    return `Name: ${this.#name}, Lastname: ${
+      this.#lastname
+    }, programming language: ${this.#programming_language}`;
+  }
+  getName() {
+    return this.#name;
+  }
+
+  getLastName() {
+    return this.#lastname;
+  }
+
+  getPLanguage() {
+    return this.#programming_language;
+  }
+}
+
+let worker = new Worker("Иван", "Иванов", "Python");
+console.log(worker.getName()); //выведет 'Иван'
+console.log(worker.getLastName()); //выведет 'Иванов'
+console.log(worker.getPLanguage()); //выведет 'Python'
+console.log(worker.introduceYourself()); //выведет 'Меня зовут Иван
+
+//////////////////////////////////////////
+// Задание №3
+// Реализуйте класс MyString, который будет иметь следующие методы: метод reverse(),
+// который параметром принимает строку, а возвращает ее в перевернутом виде, метод ucFirst(),
+// который параметром принимает строку, а возвращает эту же строку, сделав ее первую букву
+// заглавной и метод ucWords, который принимает строку и делает
+// заглавной первую букву каждого слова этой строки.
+
+class MyString {
+  constructor() {
+    //this.str = str;
+  }
+  getReverse(str) {
+    let res = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+      res += str[i];
+    }
+    return res;
+  }
+
+  ucFirst(str) {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+
+  ucWords(str) {
+    let words = str.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
+    return words.join(" ");
+  }
+}
+
+let str = new MyString();
+
+// Наш класс должен работать так:
+
+console.log(str.getReverse("abcde")); //выведет 'edcba'
+console.log(str.ucFirst("abcde")); //выведет 'Abcde'
+console.log(str.ucWords("abcde abcde abcde")); //выведет 'Abcde Abcde Abcde'
+
+//////////////////////////////////////////
+// Задание №4
+// Реализуйте класс Student (Студент), который будет наследовать от класса User.
+// Этот класс должен еть следующие свойства: name (имя, наследуется от User),
+// surname (фамилия, наследуется от User), year (год поступления в вуз).
+// Класс должен иметь метод getFullName() (наследуется от User),
+// с помощью которого можно вывести одновременно имя и фамилию студента.
+// Также класс должен иметь метод getCourse(), который будет выводить
+// текущий курс студента (от 1 до 5).
+// Курс вычисляется так: нужно от текущего года отнять год поступления в вуз.
+// Текущий год получите самостоятельно.
+class User {
+  constructor(name, surname) {
+    this.name = name;
+    this.surname = surname;
+  }
+}
+
+class Student extends User {
+  constructor(name, surname, year) {
+    super(name, surname);
+    this.year = year;
+    this.name = name;
+    this.surname = surname;
+  }
+
+  getFullName() {
+    return this.name + " " + this.surname;
+  }
+
+  getCourse() {
+    return Data.year();
+  }
+
+  getCourse() {
+    let course = new Date().getFullYear() - this.year;
+    if (course > 5) {
+      return "Student graduated";
+    } else {
+      return course;
+    }
+  }
+}
+
+// Вот так должен работать наш класс:
+
+var student = new Student("Иван", "Иванов", 2018);
+
+console.log(student.name); //выведет 'Иван'
+console.log(student.surname); //выведет 'Иванов'
+console.log(student.getFullName()); //выведет 'Иван Иванов'
+console.log(student.year); //выведет 2018c
+console.log(student.getCourse()); //выведет 3 - третий курс, так как текущий год 2021
+// Вот так должен выглядеть класс User, от которого наследуется наш Student:
+
+//////////////////////////////////////////
+// Задание №5
+// Реализуйте класс Rectangle.
+// У него должны быть следующие свойства: ширина width, высота height. Также у него должны быть
+// следующие методы: получить ширину getWidth, установить ширину setWidth, получить высоту getHeight,
+// установить высоту setHeight.
+
+class Rectangle {
+  #width;
+  #height;
+
+  constructor(width, height) {
+    this.#width = width;
+    this.#height = height;
+  }
+
+  getWidth() {
+    return this.#width;
+  }
+
+  setWidth(width) {
+    this.#width = width;
+  }
+
+  getHeight() {
+    return this.#height;
+  }
+
+  setHeight(height) {
+    this.#height = height;
+  }
+}
+
+let regt = new Rectangle(200, 100);
+console.log(regt.getHeight());
+console.log(regt.getWidth());
+regt.setHeight(300);
+regt.setWidth(400);
+console.log(regt.getHeight());
+console.log(regt.getWidth());
+
+//////////////////////////////////////////
+// Задание №6
+// Реализуйте класс Worker (Работник),
+// который будет иметь следующие свойства:
+// name (имя),
+// surname (фамилия),
+// rate (ставка за день работы),
+// days (количество отработанных дней).
+// Также класс должен иметь метод getSalary(),
+// который будет выводить зарплату работника.
+// Зарплата - это произведение (умножение)
+// ставки rate на количество отработанных дней days.
+
+class Worker1 {
+  constructor(name, surname, rate, days) {
+    (this.name = name),
+      (this.surname = surname),
+      (this.rate = rate),
+      (this.days = days);
+  }
+
+  getSalary() {
+    return this.rate * this.days;
+  }
+}
+
+// Вот так должен работать наш класс:
+var worker1 = new Worker1("Иван", "Иванов", 10, 31);
+
+console.log(worker1.name); //выведет 'Иван'
+console.log(worker1.surname); //выведет 'Иванов'
+console.log(worker1.rate); //выведет 10
+console.log(worker1.days); //выведет 31
+console.log(worker1.getSalary()); //выведет 310 - то есть 10*31
+
+//////////////////////////////////////////
+// Задание №7
+// Модифицируйте класс Worker из предыдущей задачи
+// следующим образом: для свойства rate и для
+// свойства days сделайте еще и методы-сеттеры.
+// Наш класс теперь будет работать так:
+
+class Worker2 extends Worker1 {
+  constructor(name, surname, rate, days) {
+    super(name, surname, days, rate);
+    this.name = name;
+    this.surname = surname;
+    this.days = days;
+    this.rate = rate;
+  }
+
+  getRate() {
+    return this.rate;
+  }
+
+  getDays() {
+    return this.days;
+  }
+
+  setRate(rate) {
+    this.rate = rate;
+  }
+
+  setDays(days) {
+    this.days = days;
+  }
+}
+
+var worker2 = new Worker2("Иван", "Иванов", 10, 31);
+
+console.log(worker2.getRate()); //выведет 10
+console.log(worker2.getDays()); //выведет 31
+console.log(worker2.getSalary()); //выведет 310 - то есть 10*31
+
+//Теперь используем сеттер:
+worker2.setRate(20); //увеличим ставку
+worker2.setDays(10); //уменьшим дни
+console.log(worker2.getSalary()); //выведет 200 - то есть 20*10
